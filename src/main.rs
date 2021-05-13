3135cf2be35a76055051ff2e12ae99582f11d18d
lgraham076 <langstongraham@gmail.com>
2021-05-12 23:56:36 -0500

    fixed unused variable warning

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::{SocketAddr, TcpStream};
 use std::time::Duration;
 
+extern crate num_cpus;
+
 fn main() {
+    let total_threads = num_cpus::get();
+    println!("{} threads available", total_threads);
+
     let timeout = Duration::from_secs(1);
     let ip = [127, 0, 0, 1];
     for port in 45900..46000 {
         let addr = SocketAddr::from((ip, port));
-        if let Ok(stream) = TcpStream::connect_timeout(&addr, timeout) {
+        if TcpStream::connect_timeout(&addr, timeout).is_ok() {
             println!("Connected to server!");
         } else {
             println!("Unable to connect to server!");
