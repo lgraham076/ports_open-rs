cb8b3e8f83671f26a9ec0a5e11b93edd892a38e1
lgraham076 <langstongraham@gmail.com>
2021-05-12 23:36:05 -0500

    scans ports in range for availability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::{SocketAddr, TcpStream};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let timeout = Duration::from_secs(1);
+    let ip = [127, 0, 0, 1];
+    for port in 45900..46000 {
+        let addr = SocketAddr::from((ip, port));
+        if let Ok(stream) = TcpStream::connect_timeout(&addr, timeout) {
+            println!("Connected to server!");
+        } else {
+            println!("Unable to connect to server!");
+        }
+    }
+    
 }
